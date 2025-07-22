553384610fb430c6cf4ea2cf2e6cce5fb146ea95
unfazedm <elasraoui.me@gmail.com>
2025-07-22 17:35:59 +0100

    hello

--- api/airesume.js
+++ api/airesume.js
@@ -103,7 +103,7 @@ export default async function handler(req, res) {
 
     analyzeText = analyzeText.replace(
       /"severity"\s*:\s*"[^"]*"/gi,
-      '"Severity":"Medium"'
+      '"Severity":"Low"'
     );
 
     let parsedAnalyze;
