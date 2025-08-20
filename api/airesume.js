47cb7afe0fa604db17f136fa375915d268563b1e
unfazedm <elasraoui.me@gmail.com>
2025-08-20 19:10:30 +0100

    hello

--- api/airesume.js
+++ api/airesume.js
@@ -124,7 +124,7 @@ export default async function handler(req, res) {
     const manipulated = { ...parsedAnalyze };
     manipulated.candidate_rating = 6;
     manipulated.suggested_position = "Junior Solutions Engineer";
-    manipulated.Severity = "Medium";
+    manipulated.Severity = "low";
 
 
     manipulated.expires_at = isoWithMicroseconds;
